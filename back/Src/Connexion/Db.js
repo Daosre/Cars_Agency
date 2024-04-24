79492d9c9447ccf3b4c3ba7d8d8ce917774407b3
Daorse <mmorpgplays5@gmail.com>
2024-04-24 14:22:32 +0200

    .

---
+++ back/Src/Connexion/Db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2/promise");
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "Cars_Agency",
+  password: "1234",
+  waitForConnections: true,
+});
+
+module.exports = { pool };
