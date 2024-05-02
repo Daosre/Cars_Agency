2994dff73b3a76968066113b4c01e2afbc4274fb
Daorse <mmorpgplays5@gmail.com>
2024-05-02 12:18:49 +0200

    .

--- front/Js/Admin.js
+++ front/Js/Admin.js
@@ -1,3 +1,4 @@
+
 let logout = document.querySelector('.Logout')
 
     logout.addEventListener('click', () => {
@@ -7,3 +8,25 @@ let logout = document.querySelector('.Logout')
             window.location.href = '../LogIn . SignUp/LogIn.html'
         }, 1000);
     })
+
+    async function All_Cars () {
+
+        let response = fetch('http://localhost:3108/Cars/All_Cars')
+        let client = await response.json()
+        
+        let stock = doucment.querySelector('.main-container')
+        let localUser = localStorage.getItem('jwt')
+
+        for (const Cars of client) {
+            let newCars = document.querySelector('div')
+            newCars.classList.add('Event')
+            newCars.innerHTML = `<h2 class="name">${Cars.name}</h2>
+            <img src="${Cars.image}" class="img" />
+            <p class="quantity">${Cars.quantity}</p>
+            <p class="description">${Cars.description}</p>
+            <p class="price">${Cars.price}</p>`
+            stock.appendChild(newCars)
+        }
+    }
+
+    All_Cars()
